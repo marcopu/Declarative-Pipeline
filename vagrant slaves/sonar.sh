60211e68337865c0e9e35c70ffe238b2a53f2831
marcopu <32170011+marcopu@users.noreply.github.com>
2018-02-01 17:27:38 +0100

    Caricamento macchine e configurazioni

--- vagrant slaves/sonar.sh
+++ vagrant slaves/sonar.sh
@@ -26,6 +26,12 @@ echo "Configurazione parametri SonarQube Server"
 sysctl -w vm.max_map_count=262144
 sysctl -w fs.file-max=65536
 
+#Installazione Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+sudo apt-get install -y docker-ce
+
 #Set timezone
 echo "SETTING TIMEZONE ----------"
 timedatectl set-timezone Europe/Rome
